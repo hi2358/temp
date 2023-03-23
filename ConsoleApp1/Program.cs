49cdf01294dfb8470eead34fbf4275500122bd6e
DTKHI2358\hi235 <hi2358@naver.com>
2023-03-23 17:32:21 +0900

    두번째!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("첫번째!");
+            Console.WriteLine("두번째!");
         }
     }
 }
