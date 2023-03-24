f31b6b70747daa842124923bcc420652952e78fc
DTKHI2358\hi235 <hi2358@naver.com>
2023-03-24 09:39:58 +0900

    main 네번째

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("세번째!");
+            Console.WriteLine("네번째!");
         }
     }
 }
