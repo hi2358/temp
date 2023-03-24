9daf5d4a3a5bf8b639b681a7defe3138721a532c
DTKHI2358\hi235 <hi2358@naver.com>
2023-03-24 11:24:58 +0900

    두번째 추가 커밋하기

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("충돌 후 업데이트!");
+            Console.WriteLine("두번째꺼 고치기!");
         }
     }
 }
