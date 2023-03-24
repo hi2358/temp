1a4daca78802918d709775035ba74211fe3ea6af
DTKHI2358\hi235 <hi2358@naver.com>
2023-03-24 15:31:13 +0900

    후

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("재충돌 전6");
-            Console.WriteLine("16");
+            Console.WriteLine("재충돌 후");
+            Console.WriteLine("66");
         }
     }
 }
