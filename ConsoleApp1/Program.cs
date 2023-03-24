f7995707c2ece18d8c77f4028e2833d151a4dcbb
DTKHI2358\hi235 <hi2358@naver.com>
2023-03-24 11:20:28 +0900

    충돌 후로 머지

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("충돌 전 업데이트!");
+            Console.WriteLine("충돌 후 업데이트!");
         }
     }
 }
