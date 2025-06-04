d8ef2e89bf2b32e470a42163502bf357b87d483e
조태헌 <thcho@inatech.co.kr>
2025-06-04 15:40:07 +0900

    2025.06.04 - 완료3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Update2");
+            Console.WriteLine("Test Update3");
         }
     }
 }
